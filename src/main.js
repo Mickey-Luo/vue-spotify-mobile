b48a5140de42747252fe1559563dbe2e98add07c
Mickey Luo <9395961+Mickey-Luo@users.noreply.github.com>
2022-02-27 16:19:33 +0800

    home.vue增加曲目列表

--- src/main.js
+++ src/main.js
@@ -8,8 +8,23 @@ import spotifyApi from "@/api/spotifyApi"
 Vue.prototype.$spotifyApi = spotifyApi
 
 // Vant
-import { List, Tabbar, TabbarItem, Col, Row, Icon, Grid, GridItem, Lazyload, Image as VanImage } from "vant"
+import {
+  Cell,
+  CellGroup,
+  List,
+  Tabbar,
+  TabbarItem,
+  Col,
+  Row,
+  Icon,
+  Grid,
+  GridItem,
+  Lazyload,
+  Image as VanImage
+} from "vant"
 
+Vue.use(Cell)
+Vue.use(CellGroup)
 Vue.use(List)
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
