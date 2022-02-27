8a1f187b183a1e78a30fc94a7ede20732fbf3355
Mickey Luo <mickey-luo@foxmail.com>
2022-02-27 12:01:22 +0800

    获取token功能，访问api功能

---
+++ src/views/get-token.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>加载中...</div>
+</template>
+
+<script>
+import spotifyApi from "@/api/index"
+export default {
+  created() {
+    spotifyApi.setAccessToken(this.$route.params)
+    this.$router.push("/home")
+  }
+}
+</script>
+
+<style lang="less"></style>
--- src/views/search.vue
+++ src/views/search.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="@/assets/logo.png" />
-    2
-  </div>
+  <div>2</div>
 </template>
 
 <script>
-export default {}
+import spotifyApi from "../api/index"
+
+export default {
+  created() {
+    spotifyApi.getArtistAlbums("43ZHCT0cAZBISjO8DG9PnE", function (err, data) {
+      if (err) console.error(err)
+      else console.log("Artist albums", data)
+    })
+  }
+}
 </script>
 
 <style></style>
