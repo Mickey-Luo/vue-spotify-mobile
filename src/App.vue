f570dc3c044a4bfc2576ef5307f29d2ac55859f8
Mickey Luo <mickey-luo@foxmail.com>
2022-02-27 16:19:33 +0800

    home.vue增加曲目列表

--- src/App.vue
+++ src/App.vue
@@ -10,34 +10,34 @@
 </template>
 
 <script>
-export default {
-  created() {
-    // 如果本地有token,配置给api模块
-    if (localStorage.getItem("vant_spotify_token")) {
-      console.log("已经获得token")
-      this.$spotifyApi.setAccessToken(localStorage.getItem("vant_spotify_token"))
-    } else {
-      // 如果都没有token,跳转到认证页面
-      location.href = "http://localhost:8888"
-    }
-  },
-  name: "App",
-  components: {},
-  data() {
-    return {
-      active: 0
+  export default {
+    created() {
+      // 如果本地有token,配置给api模块
+      if (localStorage.getItem("vant_spotify_token")) {
+        console.log("已经获得token")
+        this.$spotifyApi.setAccessToken(localStorage.getItem("vant_spotify_token"))
+      } else {
+        // 如果都没有token,跳转到认证页面
+        location.href = "http://localhost:8888"
+      }
+    },
+    name: "App",
+    components: {},
+    data() {
+      return {
+        active: 0
+      }
     }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  /* #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  } */
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,42 +2,90 @@
   <div id="home">
     <van-row type="flex" justify="space-between">
       <van-col span="6">晚上好</van-col>
-      <van-col span="6"><van-icon name="bullhorn-o" /><van-icon name="clock-o" /><van-icon name="setting-o" /></van-col>
+      <van-col span="6"
+        ><van-icon name="bullhorn-o" /><van-icon name="clock-o" /><van-icon name="setting-o"
+      /></van-col>
     </van-row>
-
-    <div class="track-list" clickable :gutter="10" :column-num="1">
-      <van-row class="track-item" v-for="item in mySavedTracks.items" :key="item.track.id" type="flex">
-        <van-col span="6"><van-image :src="item.track.album.images[1].url" height="100px" fit="cover" /></van-col>
-        <van-col class="track-name" span="18">{{ item.track.name }}</van-col>
-      </van-row>
-    </div>
-    <div></div>
+  <!-- 曲目列表 -->
+    <van-list
+      class="track-list"
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+    <!-- 曲目单元 -->
+      <van-cell class="track-item" v-for="(item, index) in list" v-lazy="item" :key="index">
+        <van-row type="flex" justify="space-between">
+          <van-col span="6">
+            <van-image :src="item.track.album.images[1].url" fit="cover" />
+          </van-col>
+          <van-col class="track-detail van-ellipsis" span="17">
+            <span class="track-name">{{ item.track.name }}</span>
+            <div class="artist-name-container">
+              <span class="artist-name" v-for="item in item.track.artists" :key="item.id">
+                {{ item.name }}
+              </span>
+            </div>
+          </van-col>
+        </van-row>
+      </van-cell>
+    </van-list>
   </div>
 </template>
 
 <script>
-export default {
-  created() {
-    this.$spotifyApi.getMySavedTracks({}, (err, data) => {
-      if (err) console.error(err)
-      else {
-        console.log("MySavedTracks", data)
-        this.mySavedTracks = data
+  export default {
+    created() {},
+    data() {
+      return {
+        mySavedTracks: {},
+        list: [],
+        offset: 0,
+        loading: false,
+        finished: false
       }
-    })
-  },
-  data() {
-    return {
-      mySavedTracks: {}
+    },
+    methods: {
+      onLoad() {
+        // 异步更新数据
+        console.log("异步更新数据")
+        this.$spotifyApi.getMySavedTracks({ offset: this.offset, limit: 20 }, (err, data) => {
+          if (err) console.error(err)
+          else {
+            console.log(data.items)
+
+            // 添加数据到数组
+            this.list.push(...data.items)
+
+            // 加载状态结束
+            this.loading = false
+
+            // 数据全部加载完成
+            if (data.length < 2) {
+              this.finished = true
+            }
+          }
+        })
+        // 请求完后
+        this.offset += 20
+      }
     }
   }
-}
 </script>
 
 <style lang="less">
-.track-list {
-  .track-item {
-    height: 100px;
+  .track-list {
+    .track-item {
+      height: 110px;
+      .track-detail {
+        // margin-left: 8px;
+        .artist-name-container {
+          .artist-name + .artist-name::before {
+            content: "·";
+          }
+        }
+      }
+    }
   }
-}
 </style>
