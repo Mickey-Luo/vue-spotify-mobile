53f488f727f188b4de6f1ae88c78f05109887e43
Mickey Luo <9395961+Mickey-Luo@users.noreply.github.com>
2022-03-01 20:09:55 +0800

    修复获取token过程的bug 5

--- src/App.vue
+++ src/App.vue
@@ -19,16 +19,18 @@
     },
     mounted() {
       // 如果本地有token,配置给api模块
-      if (!this.$route.path.startsWith("/get-token")) {
-        if (localStorage.getItem("vant_spotify_token")) {
-          console.log("已经获得token")
-          this.$spotifyApi.setAccessToken(localStorage.getItem("vant_spotify_token"))
-        } else {
-          // 如果都没有token,跳转到认证页面
-          console.log("没有获得token")
-          location.href = "http://124.221.148.61:8000"
+      this.$nextTick(() => {
+        if (!this.$route.path.startsWith("/get-token")) {
+          if (localStorage.getItem("vant_spotify_token")) {
+            console.log("已经获得token")
+            this.$spotifyApi.setAccessToken(localStorage.getItem("vant_spotify_token"))
+          } else {
+            // 如果都没有token,跳转到认证页面
+            console.log("没有获得token")
+            location.href = "http://124.221.148.61:8000"
+          }
         }
-      }
+      })
     },
     beforeUpdate() {
       if (this.$route.path.startsWith("/home")) {
